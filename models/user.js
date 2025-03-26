96a0c6cfac0bfe69511edc61fb290415ad569d64
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-26 13:58:12 +0100

    User model added

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../database/sequelize');
+
+class User extends Model { }
+
+User.init(
+  {
+    // Model attributes are defined here
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    media: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    gender: {
+      type: DataTypes.ENUM('Male', 'Female'),
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  },
+  {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User', // We need to choose the model name
+    tableName: 'Users'
+  },
+);
+
+module.exports = User;
