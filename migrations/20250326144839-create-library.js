32076a122400836f8c82b6539a3ad09b28518443
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-26 16:25:46 +0100

    Added Library migration file

---
+++ migrations/20250326144839-create-library.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Libraries', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      genre: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      year: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Libraries');
+  }
+};
