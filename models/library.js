9519d66921482d687fd4fc45234427f7eadb798c
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-26 16:25:19 +0100

    Added Library model

---
+++ models/library.js
@@ -0,0 +1,60 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../database/sequelize');
+const User = require('./user');
+
+class Library extends Model { }
+
+Library.init(
+  {
+    // Model attributes are defined here
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  },
+  {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'Library', // We need to choose the model name
+    tableName: 'Libraries'
+  },
+);
+
+
+module.exports = Library;
