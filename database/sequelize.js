7381b0606b32f566f58370e080f109821f88f204
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-26 14:00:25 +0100

    Added sequelize

---
+++ database/sequelize.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+const DB = process.env.DATABASE_NAME;
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const host = process.env.DATABASE_HOST;
+const dialect = process.env.DATABASE_DIALECT;
+
+const sequelize = new Sequelize(DB, username, password, {
+    host: host,
+    dialect: dialect
+  });
+
+  module.exports = sequelize
