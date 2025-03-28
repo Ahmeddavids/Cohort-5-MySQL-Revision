b10d3465fbbf539c71d7d2ee1f6d2e501bd943b8
ahmeddavids <ahmeddavids6@gmail.com>
2025-03-28 16:02:24 +0100

    Updated user router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,4 @@
-const { createUser, login } = require('../controllers/userController');
+const { createUser, login, makeAdmin } = require('../controllers/userController');
 const upload = require('../utils/multer');
 
 const router = require('express').Router();
@@ -6,5 +6,7 @@ const router = require('express').Router();
 router.post('/sign-up', upload.single('media'), createUser);
 
 router.post('/login', login);
+
+router.put('/make-admin/:id', makeAdmin);
 
 module.exports = router
